89e74f5fc2f21a51b607538ffb8e92dc2dfc3ae7
mildtotoro <pimrampha.m@gmail.com>
2019-10-01 01:01:40 +0700

    run export

---
+++ docs/_next/static/webpack/static/development/pages/index.js.b6c5772d4efc01da3d79.hot-update.js
@@ -0,0 +1,131 @@
+webpackHotUpdate("static/development/pages/index.js",{
+
+/***/ "./components/home/slider.js":
+/*!***********************************!*\
+  !*** ./components/home/slider.js ***!
+  \***********************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "default", function() { return Slider; });
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var react_bootstrap__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! react-bootstrap */ "./node_modules/react-bootstrap/es/index.js");
+/* harmony import */ var _assets_images_home_slide_image1_jpg__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../../assets/images/home-slide/image1.jpg */ "./assets/images/home-slide/image1.jpg");
+/* harmony import */ var _assets_images_home_slide_image1_jpg__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(_assets_images_home_slide_image1_jpg__WEBPACK_IMPORTED_MODULE_2__);
+/* harmony import */ var _assets_images_home_slide_image2_jpg__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ../../assets/images/home-slide/image2.jpg */ "./assets/images/home-slide/image2.jpg");
+/* harmony import */ var _assets_images_home_slide_image2_jpg__WEBPACK_IMPORTED_MODULE_3___default = /*#__PURE__*/__webpack_require__.n(_assets_images_home_slide_image2_jpg__WEBPACK_IMPORTED_MODULE_3__);
+/* harmony import */ var _assets_images_home_slide_image3_jpg__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! ../../assets/images/home-slide/image3.jpg */ "./assets/images/home-slide/image3.jpg");
+/* harmony import */ var _assets_images_home_slide_image3_jpg__WEBPACK_IMPORTED_MODULE_4___default = /*#__PURE__*/__webpack_require__.n(_assets_images_home_slide_image3_jpg__WEBPACK_IMPORTED_MODULE_4__);
+var _jsxFileName = "/Users/apple/Desktop/work/apeservice/components/home/slider.js";
+
+
+
+
+
+class Slider extends react__WEBPACK_IMPORTED_MODULE_0___default.a.Component {
+  render() {
+    return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(react_bootstrap__WEBPACK_IMPORTED_MODULE_1__["Carousel"], {
+      __source: {
+        fileName: _jsxFileName,
+        lineNumber: 10
+      },
+      __self: this
+    }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(react_bootstrap__WEBPACK_IMPORTED_MODULE_1__["Carousel"].Item, {
+      __source: {
+        fileName: _jsxFileName,
+        lineNumber: 11
+      },
+      __self: this
+    }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("img", {
+      className: "d-block w-100",
+      src: _assets_images_home_slide_image2_jpg__WEBPACK_IMPORTED_MODULE_3___default.a,
+      alt: "apeservice",
+      __source: {
+        fileName: _jsxFileName,
+        lineNumber: 12
+      },
+      __self: this
+    }), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(react_bootstrap__WEBPACK_IMPORTED_MODULE_1__["Carousel"].Caption, {
+      bsPrefix: "text-light carousel-caption",
+      __source: {
+        fileName: _jsxFileName,
+        lineNumber: 13
+      },
+      __self: this
+    })), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(react_bootstrap__WEBPACK_IMPORTED_MODULE_1__["Carousel"].Item, {
+      __source: {
+        fileName: _jsxFileName,
+        lineNumber: 18
+      },
+      __self: this
+    }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("img", {
+      className: "d-block w-100",
+      src: _assets_images_home_slide_image1_jpg__WEBPACK_IMPORTED_MODULE_2___default.a,
+      alt: "apeservice",
+      __source: {
+        fileName: _jsxFileName,
+        lineNumber: 19
+      },
+      __self: this
+    }), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(react_bootstrap__WEBPACK_IMPORTED_MODULE_1__["Carousel"].Caption, {
+      bsPrefix: "text-light carousel-caption",
+      __source: {
+        fileName: _jsxFileName,
+        lineNumber: 21
+      },
+      __self: this
+    })), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(react_bootstrap__WEBPACK_IMPORTED_MODULE_1__["Carousel"].Item, {
+      __source: {
+        fileName: _jsxFileName,
+        lineNumber: 26
+      },
+      __self: this
+    }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("img", {
+      className: "d-block w-100",
+      src: _assets_images_home_slide_image3_jpg__WEBPACK_IMPORTED_MODULE_4___default.a,
+      alt: "apeservice",
+      __source: {
+        fileName: _jsxFileName,
+        lineNumber: 27
+      },
+      __self: this
+    }), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(react_bootstrap__WEBPACK_IMPORTED_MODULE_1__["Carousel"].Caption, {
+      bsPrefix: "text-primary carousel-caption bg-light",
+      __source: {
+        fileName: _jsxFileName,
+        lineNumber: 29
+      },
+      __self: this
+    }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+      className: "d-none d-lg-block",
+      __source: {
+        fileName: _jsxFileName,
+        lineNumber: 30
+      },
+      __self: this
+    }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("h1", {
+      className: "",
+      __source: {
+        fileName: _jsxFileName,
+        lineNumber: 31
+      },
+      __self: this
+    }, "APE Service"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("h2", {
+      className: "d-none d-lg-block",
+      __source: {
+        fileName: _jsxFileName,
+        lineNumber: 32
+      },
+      __self: this
+    }, "\u0E23\u0E31\u0E1A\u0E2D\u0E2D\u0E01\u0E41\u0E1A\u0E1A \u0E15\u0E34\u0E14\u0E15\u0E31\u0E49\u0E07 \u0E08\u0E33\u0E2B\u0E19\u0E48\u0E32\u0E22\u0E40\u0E04\u0E23\u0E37\u0E48\u0E2D\u0E07\u0E1B\u0E23\u0E31\u0E1A\u0E2D\u0E32\u0E01\u0E32\u0E28 \u0E1A\u0E23\u0E34\u0E01\u0E32\u0E23\u0E25\u0E49\u0E32\u0E07-\u0E0B\u0E48\u0E2D\u0E21\u0E41\u0E2D\u0E23\u0E4C \u0E02\u0E32\u0E22\u0E19\u0E49\u0E33\u0E22\u0E32\u0E41\u0E2D\u0E23\u0E4C\u0E41\u0E25\u0E30\u0E2D\u0E30\u0E44\u0E2B\u0E25\u0E48 \u0E42\u0E17\u0E23 02-749-1230-2")))));
+  }
+
+}
+
+/***/ })
+
+})
+//# sourceMappingURL=index.js.b6c5772d4efc01da3d79.hot-update.js.map
