bedc3f1f9d18d2a85d45afc9f5d94f3a55f9b42a
mildtotoro <pimrampha.m@gmail.com>
2019-10-06 00:22:52 +0700

    update showcase toy r us page

--- pages/showcase/toy-r-us.js
+++ pages/showcase/toy-r-us.js
@@ -6,44 +6,40 @@ const assetPrefix = process.env.NODE_ENV === 'production' ? '/apeservice' : '';
 class toyRUs extends React.Component {
 
     gellery = () => {
-        let gallery = "";
+        let gallery = [];
         for (let i = 1; i <= 10; i++) {
             const imgPath = assetPrefix + '/static/assets/showcase/toy-r-us/' + i + '.jpg';
-            gallery = gallery + <img width="70px" src={imgPath} alt='' />
+            gallery.push(<div className="col-12 col-md-6 col-lg-4"><img className="img-fluid" key={imgPath} src={imgPath} alt='' /></div>);
         }
-        console.log({gallery})
+        console.log({ gallery })
         return gallery;
     }
 
     render() {
-        return(
+        return (
             <Layout>
-        <div className="container">
-            <div className="row">
-                <div className="col py-5">
-                    {/* <h2>ตัวอย่างผลงาน</h2> */}
-                    <h4>
-                        ร้านทอยส์ อาร์ อัส คาร์ฟูพระราม4 ติดตั้งระบบปรับอากาศ และ ระบบท่อส่งลมเย็น
-    
-                            </h4>
-    
-                    <h5>รายละเอียดของงาน</h5>
-                    <p>
-                        คำนวณหาขนาดเครื่องปรับอากาศ และ ออกแบบท่อส่งลมเย็น
-                        ติดตั้งท่อน้ำเย็น และ อุปกรณ์วาล์วต่างๆเข้าเครื่องปรับอากาศ ขนาดทำความเย็น 361,000 บีทียู/ชั่วโมง ปริมาณลม 15,000 คิวบิคฟุต/นาที ทั้งหมด 2 เครื่อง
-                        ติดตั้งท่อส่งลมเย็นหุ้มฉนวนclose cell ความหนา 3/4” พร้อมทั้งติดตั้งหัวจ่ายลม และ ช่องลมกลับ มูลค่างานทั้งหมด 1,331,178.48 บาท (ก่อนภาษีมูลค่าเพิ่ม)
+                <div className="container">
+                    <div className="row">
+                        <div className="col py-5">
+                            <h3>
+                                ร้านทอยส์ อาร์ อัส คาร์ฟูพระราม4
+                            </h3>
+                            <h4 className="mt-2">ติดตั้งระบบปรับอากาศ และ ระบบท่อส่งลมเย็น</h4>
+                            <p className="mt-3">
+                                คำนวณหาขนาดเครื่องปรับอากาศ และ ออกแบบท่อส่งลมเย็น
+                                ติดตั้งท่อน้ำเย็น และ อุปกรณ์วาล์วต่างๆเข้าเครื่องปรับอากาศ ขนาดทำความเย็น 361,000 บีทียู/ชั่วโมง ปริมาณลม 15,000 คิวบิคฟุต/นาที ทั้งหมด 2 เครื่อง
+                                ติดตั้งท่อส่งลมเย็นหุ้มฉนวนclose cell ความหนา 3/4” พร้อมทั้งติดตั้งหัวจ่ายลม และ ช่องลมกลับ มูลค่างานทั้งหมด 1,331,178.48 บาท (ก่อนภาษีมูลค่าเพิ่ม)
                             </p>
-                    <div className="brand">
-                        {this.gellery()}
-                        {/* <img width="70px" src={assetPrefix + '/static/assets/showcase/toy-r-us/' + index + '.png'} alt={'apeservice partner' + product.brand} /> */}
+                            <div className="row no-gutters">
+                                {this.gellery()}
+                            </div>
+
+                        </div>
                     </div>
-    
                 </div>
-            </div>
-        </div>
             </Layout >
         );
     }
 }
 
- export default toyRUs;
+export default toyRUs;
